f66e88f37d39214a59b883cb600e33b2f653ea19
Godday Okoduwa <goddayokos@gmail.com>
2021-10-30 15:44:45 +0100

    Setup the domain layer and utility functions which which would be common for all the modules

---
+++ common/src/main/java/dev/decagon/godday/common/domain/Exception.kt
@@ -0,0 +1,14 @@
+package dev.decagon.godday.common.domain
+
+
+/**
+ * This is exception would be thrown when there are no more comics
+ * on the endpoint i.e when the user has browsed to the end of the comics list
+ */
+class NoMoreXkcdComicsException(message: String): Exception(message)
+
+
+/**
+ * This exception would be thrown when we encounter network error
+ */
+class NetworkException(message: String) : Exception(message)
---
+++ common/src/main/java/dev/decagon/godday/common/utils/CoroutineDispatcherProvider.kt
@@ -0,0 +1,4 @@
+package dev.decagon.godday.common.utils
+
+
+class CoroutineDispatchersProvider : DispatchersProvider
