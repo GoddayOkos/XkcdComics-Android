19dad596905123de904a4dbc3c00596b436a92e0
Godday Okoduwa <goddayokos@gmail.com>
2021-10-30 23:37:21 +0100

    Removed common module

--- common/src/main/java/dev/decagon/godday/common/data/api/model/mapper/ApiMapper.kt
+++
@@ -1,9 +0,0 @@
-package dev.decagon.godday.common.data.api.model.mapper
-
-/**
- * This is used for mapping API responses (DTO's) models into
- * domain models
- */
-interface ApiMapper<E, D> {
-    fun mapToDomain(apiDTO: E): D
-}
--- common/src/main/java/dev/decagon/godday/common/utils/CoroutineDispatcherProvider.kt
+++
@@ -1,6 +0,0 @@
-package dev.decagon.godday.common.utils
-
-import javax.inject.Inject
-
-
-class CoroutineDispatchersProvider @Inject constructor(): DispatchersProvider
